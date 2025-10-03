0ad5457a48ef210b79e4f988192e323c649a4ffe
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-10-03 13:49:13 +0100

    Added footer componenet and made some changes to hame banner scss

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.module.scss";
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+     <footer>
+        <p>© 2025 dunnhumby</p>
+    </footer>   
+    )
+}
+
+export default Footer;
