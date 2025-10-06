233674d18581be1e1deca1e43a6f1a20cbd13078
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-10-06 11:25:59 +0100

    Added functionality for filter

--- frontend/src/components/Filter/Filter.tsx
+++ frontend/src/components/Filter/Filter.tsx
@@ -1,11 +1,48 @@
 import styles from "./Filter.module.scss";
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Filter: React.FC = () => {
+  // state for opening filter
+  const [open, setOpen] = useState(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  // closes filter menu when user clicks elsewhere on page
+  useEffect(() => {
+    const close = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("click", close);
+
+    return () => {
+      document.removeEventListener("click", close);
+    };
+  }, []);
+
   return (
-    <section className={styles.filterContainer}>
-      <div className={styles.filter}>Filter ↓</div>
-    </section>
+    <>
+      <section className={styles.filterSection}>
+        <div
+          className={styles.filterContainer}
+          ref={ref}
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className={styles.filter}>Filter</div>
+          <div className={styles.arrow}>↓</div>
+        </div>
+      </section>
+      {open && (
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
