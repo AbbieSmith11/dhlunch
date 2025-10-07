ae767ef46dfd3f69f80064707911e9e163f13623
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-10-07 16:36:42 +0100

    Added star logic to display correct amount of stars as per rating input

--- frontend/src/components/RestaurantSummary/RestaurantSummary.tsx
+++ frontend/src/components/RestaurantSummary/RestaurantSummary.tsx
@@ -1,4 +1,5 @@
 import styles from "./RestaurantSummary.module.scss";
+import { useState, useEffect} from "react";
 
 type RestaurantSummaryInfo = {
     name: string;
@@ -7,11 +8,32 @@ type RestaurantSummaryInfo = {
 }
 
 const RestaurantSummary = ({name, image, rating}: RestaurantSummaryInfo) => {
+
+    // variable for stars
+    const [stars, setStars] = useState("⭐️");
+
+    //sets the right amount of stars based on rating (from props) number 
+    useEffect(() => {
+        if(rating == 2){
+            setStars("⭐️⭐️");
+        }
+        else if (rating == 3){
+            setStars("⭐️⭐️⭐️");
+        }
+        else if (rating == 4){
+            setStars("⭐️⭐️⭐️⭐️")
+        }
+        else if (rating >= 5){
+            setStars("⭐️⭐️⭐️⭐️⭐️");
+        }
+    },[rating]);//re-render when rating is updated
+
+
     return (
         <div className={styles.RestaurantSummaryContainer}>
             <div className={styles.infoContainers}><img src={image}/></div>
             <div className={styles.infoContainers}>{name}</div>
-            <div className={styles.infoContainers}>{rating}</div>
+            <div className={styles.infoContainers}>{stars}</div>
         </div>
     )
 }
